eb95630188e20a3c5e37fb5f46de2331eface20c
entitycs <entitycs@gmail.com>
2026-05-13 18:39:56 -0600

    Feat: ContactHandler for more dynamic collision detection building/testing

---
+++ core/contact.lua
@@ -0,0 +1,75 @@
+local ContactHandler = {}
+ContactHandler.__index = ContactHandler
+
+-- Add Begin callback
+--- func desc
+---@param nameFixtureA string
+---@param nameFixtureB string
+---@param handler function
+function ContactHandler:addBegin(nameFixtureA, nameFixtureB, handler)
+    table.insert(self.handlers.begin, {names = {nameFixtureA, nameFixtureB}, handler = handler})
+end
+
+-- Remove Begin callback
+function ContactHandler:removeBegin(handler)
+    for i = #self.handlers.begin, 1, -1 do
+        if self.handlers.begin[i].handler == handler then
+            table.remove(self.handlers.begin, i)
+            break
+        end
+    end
+end
+
+function ContactHandler.new(world)
+    
+    local handlers = {
+        begin = {}
+    }
+    local function beginContact(fixtureA, fixtureB, contact)
+        if fixtureA == nil or fixtureB == nil then return end
+        local ud_a = fixtureA:getUserData() or nil
+        local ud_b = fixtureB:getUserData() or nil
+        if ud_a ~= nil then
+            for k, v in pairs(ud_a) do
+                print(k, v)
+            end
+        end
+        if ud_b ~= nil then
+            for k, v in pairs(ud_b) do
+                print(k, v)
+            end
+        end
+        -- Handle collisions between 'Front' and 'Ball'
+        for i, o in ipairs(handlers.begin) do
+            -- copy fixture names for handler
+            local names = {o.names[1], o.names[2]}
+            local handler = o.handler
+            -- remove from local fixture names copy if matched
+            if (ud_a ~= nil and ud_b ~= nil) then
+                for j = #names, 1, -1 do
+                    if names[j] == ud_a["name"] or names[j] == ud_b["name"] then
+                        table.remove(names, j)
+                    end
+                end
+                for j = #names, 1, -1 do
+                    if names[j] == ud_a["name"] or names[j] == ud_b["name"] then
+                        table.remove(names, j)
+                    end
+                end
+                -- matched if both names found (removed from local copy)
+                if #names == 0 then
+                    handler(fixtureA, fixtureB, contact)
+                end
+            end
+        end
+        -- do not use contact after this function returns
+    end
+
+    return setmetatable({
+        world = world,
+        handlers = handlers,
+        beginContact = beginContact
+    }, ContactHandler)
+end
+
+return ContactHandler
