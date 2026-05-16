de692a8187280d4e01d56e73aa5fe1733dc35566
entitycs <entitycs@gmail.com>
2026-05-16 01:18:18 -0600

    Update:Refactor: Move front+ball collision handler from main to player.front. Removed unused requires from main.

--- main.lua
+++ main.lua
@@ -3,45 +3,39 @@ require("current.arena")
 Arena = require("core.arena")
 JoystickInput = require("core.joystick")
 local Ball = require("core.ball")
-Moonshine = require("lib.moonshine")
-Shockwave = require("agent.copilot.shockwave")
+-- Moonshine = require("lib.moonshine")
+-- Shockwave = require("agent.copilot.shockwave")
 local Rear = require("player.rear")
 local Front = require("player.front")
 local ContactHandler = require("core.contact")
 
 local logger = {}
+
+
 function love.conf(t)
     t.console = true
 end
-local signifiers = {
-    effects = {}
-}
+
+-- local signifiers = {
+--     effects = {}
+-- }
 
 
-function signifiers.testCallback(fixtureA, fixtureB, contact)
+-- function signifiers.testCallback(fixtureA, fixtureB, contact)
 
-end
+-- end
 
+-- function signifiers.collisionOnEnter(fixture_a, fixture_b, contact)
+--     -- Handle collisions between 'Front' and 'Ball'
+--     local point = { contact:getPositions() }
+--     for i = 1, #point, 2 do
+--         local x, y = point[i], point[i + 1]
+--         -- Cache the values inside the (volatile) Contacts (fron love docs)
+--         table.insert(signifiers.effects, Shockwave.new(x, y))
+--     end
+--     -- do not use contact after this function returns
+-- end
 
-function signifiers.collisionOnEnter(fixture_a, fixture_b, contact)
-    local ud_a = fixture_a:getUserData()
-    local ud_b = fixture_b:getUserData()
-
-    -- Handle collisions between 'Front' and 'Ball'
-    if (ud_a ~= nil and ud_b ~= nil) and ((ud_a["name"] == "Front") or (ud_b["name"] == "Front")) then
-        if (ud_a["name"] == "Ball") or (ud_b["name"] == "Ball") then
-
-            local point = {contact:getPositions()}
-            for i = 1, #point, 2 do
-                local x, y = point[i], point[i + 1]
-                -- Cache the values inside the Contacts because they're not guaranteed
-                -- to be valid later in the frame. (fron love docs)
-                table.insert(signifiers.effects, Shockwave.new(x, y))
-            end 
-        end
-    end
-    -- do not use contact after this function returns
-end
 local window = {}
 local world
 
@@ -49,7 +43,7 @@ local function reloadBall(fixtureA, fixtureB, contact)
     Ball:drop(window.right / 2 - Ball.shape:getRadius() / 2, 30)
 end
 
-local center = {}   -- connector
+local center = {} -- connector
 
 function love.load()
     love.physics.setMeter(64)
@@ -65,33 +59,26 @@ function love.load()
 
     -- Create new World
     world = love.physics.newWorld(0, 90, true)
+
     -- Set contact handling callback
     local contactHandler = ContactHandler.new(world)
     world:setCallbacks(contactHandler.beginContact)
-    
+
     -- Add to contact handling callback list, post-set!
-    contactHandler:addBegin("Front", "Ball", signifiers.collisionOnEnter)
     contactHandler:addBegin("Ball", "Goal1", reloadBall)
     contactHandler:addBegin("Ball", "Goal2", reloadBall)
 
-
-    Shockwave:load()
-
-    -- In love.load(), after creating fixtures:
     local wheelSize = 10
+
+    -- Ball
     local ballRadius = BallSize(wheelSize)
-    -- Arena
-    Arena:load(window, world)
-
-    -- Per 'Round/Game' Arena
-    CurrentArena:load(window, world, ballRadius)
-
     Ball:load(window, world, ballRadius)
+
     -- Player
     -- - Rear 'Wheel'
     Rear:load(window, world, wheelSize)
     -- - Front 'Wheel'
-    Front:load(window, world, Rear)
+    Front:load(window, world, Rear, contactHandler)
 
     -- 'Center/Body' (no collision)
     center.joint = love.physics.newDistanceJoint(
@@ -100,38 +87,38 @@ function love.load()
         Front.body:getX(), Front.body:getY(),
         false -- do not collide with each other
     )
+
+    -- Arena
+    Arena:load(window, world)
+
+    -- Per 'Round/Game' Arena
+    CurrentArena:load(window, world, ballRadius)
+
     Directions = { "up", "down", "left", "right" }
     DirectionTargets = { "y", "y", "x", "x" }
 end
 
-
 function love.update(dt)
-    world:update(dt)
     local limitingVel = 400
+
+    -- update world
+    world:update(dt)
 
     -- update ball
     Ball:update(dt)
-
-    -- backwards iteration - safer for removals
-    for i = #signifiers.effects, 1, -1 do 
-        local e = signifiers.effects[i]
-        e:update(dt)
-        if e.dead then
-            table.remove(signifiers.effects, i)
-        end
-    end
 
     -- joystick input (up to date)
     JoystickInput:update(dt, Rear, Front)
     Rear:update(dt)
     Front:update(dt)
+
     -- keyboard input (lagging behind)
     -- loop through directions
     local limitedX = false
     local limitedY = false
     local curVel_x, curVel_y = Rear.body:getLinearVelocity()
 
-    -- determine player acceleration limits based on current velocity
+    -- determine player acceleration limits/constraints based on current velocity
     if math.abs(curVel_x) + math.abs(curVel_y) > limitingVel then
         if math.abs(curVel_x) > math.abs(curVel_y) then
             limitedX = true
@@ -140,12 +127,13 @@ function love.update(dt)
         end
     end
     -- loop through (poll) defined keyboard inputs as directions
-    for i, dir in ipairs(Directions) do -- todo, sep. loop for joystick
+    for i, dir in pairs(Directions) do -- todo, sep. loop for joystick
         local dirdt = dt * Rear.force
         if love.keyboard.isDown(dir) then
             if i % 2 == 1 then
                 dirdt = -dirdt -- Reverse direction if odd
             end
+            -- Only apply implulse if not limited by velocity constraint
             if DirectionTargets[i] == "x" and not limitedX then
                 Rear.body:applyLinearImpulse(dirdt, 0)
             elseif not limitedY then
@@ -155,31 +143,25 @@ function love.update(dt)
     end
 end
 
-
 function love.draw()
     -- Wipe the screen
     love.graphics.clear(0.1, 0.1, 0.12)
     love.graphics.setColor(.5, .5, .5)
-       
+
     -- Draw Arena (Backdrop => first)
     CurrentArena:draw()
-    
-    -- Draw Arena 
+
+    -- Draw Arena
     Arena:draw()
     local curVel_x, curVel_y = Rear.body:getLinearVelocity()
     local angVel = Rear.body:getAngularVelocity()
     love.graphics.print("LinearVelocity: " .. curVel_x + curVel_y, 200, 120)
     love.graphics.print("AngularVelocity: " .. angVel, 200, 140)
-    
+
     -- Draw rear wheel
     Rear:draw()
-    
-    -- effect(function()
-    -- )
-    -- end)
     -- Draw Front Wheel
     Front:draw()
-
     -- Draw the line between them
     love.graphics.setColor(1, 1, 0.4)
     love.graphics.setLineWidth(3)
@@ -187,19 +169,10 @@ function love.draw()
         Rear.body:getX(), Rear.body:getY(),
         Front.body:getX(), Front.body:getY()
     )
-    
+
     -- Draw ball
     Ball:draw()
-    -- love.graphics.setColor(1, 1, 1)
-    -- love.graphics.circle("fill",
-    -- ball.body:getX(),
-    -- ball.body:getY(),
-    --     ball.shape:getRadius()
-    -- )
-    
-    -- Joystick debug
+
+    -- Draw joystick (ie. debugging)
     JoystickInput:draw()
-    for _, e in ipairs(signifiers.effects) do
-        e:draw()
-    end
 end
--- player/front.lua
+++ player/front.lua
@@ -1,7 +1,22 @@
 local Front = {}
+
+local Shockwave = require("agent.copilot.shockwave")
 local RotVis = require("agent.glm.rotvis")
 local FrontRotVis = nil
-function Front:load(window, world, rear)
+local effects = {}
+
+function Front:collisionOnEnter(fixture_a, fixture_b, contact)
+    -- Handle collisions between 'Front' and 'Ball'
+    local point = { contact:getPositions() }
+    for i = 1, #point, 2 do
+        local x, y = point[i], point[i + 1]
+        -- Cache the values inside the (volatile) Contacts (fron love docs)
+        table.insert(effects, Shockwave.new(x, y))
+    end
+    -- do not use contact after this function returns
+end
+
+function Front:load(window, world, rear, contactHandler)
     self.size = rear.size
     self.torque = FrontWheelTorque(rear.force) --175 * dimmer
     self.body = love.physics.newBody(world, rear.x + self.size * 6, rear.y, "dynamic")
@@ -9,7 +24,7 @@ function Front:load(window, world, rear)
     self.fixture = love.physics.newFixture(self.body, self.shape, 1)
     self.fixture:setRestitution(0.85)
     self.fixture:setFriction(0.0)
-    self.fixture:setUserData({name = "Front"})
+    self.fixture:setUserData({ name = "Front" })
     self.body:setLinearDamping(1)
     self.body:setMass(FrontWheelMass(rear.body:getMass()))
     self.body:setGravityScale(FrontWheelGravityScale(rear.body:getGravityScale()))
@@ -17,11 +32,22 @@ function Front:load(window, world, rear)
     -- rotation visualization
     local name = "FrontLine"
     FrontRotVis = RotVis:new(self.size, self.body, name)
-    -- FrontRotVis:load()
+     
+    -- contact visualization
+    contactHandler:addBegin("Front", "Ball", function(a, b, contact)
+        return self:collisionOnEnter(a, b, contact)
+    end)
+
 end
 
 function Front:update(dt, window, world)
-
+    for i = #effects, 1, -1 do 
+        local e = effects[i]
+        e:update(dt)
+        if e.dead then
+            table.remove(effects, i)
+        end
+    end
 end
 
 function Front:draw()
@@ -31,9 +57,17 @@ function Front:draw()
         self.body:getY(),
         self.size --shape:getRadius()
     )
+
+    -- draw line showing rotation
     love.graphics.setColor(0, 0, 0)
     if FrontRotVis ~= nil then FrontRotVis:draw() end
+
     love.graphics.setColor(1, 1, 1)
+    -- Draw Particle Effects
+    for _, e in pairs(effects) do
+        e:draw()
+    end
+
 end
 
 return Front
