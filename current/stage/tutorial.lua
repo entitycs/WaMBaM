a62b26bb5fbf3bfa86ad135fc2c6d104df4e8bf2
entitycs <entitycs@gmail.com>
2026-06-08 01:30:25 -0600

    Update: re-drop tutorial ball if below floor

--- current/stage/tutorial.lua
+++ current/stage/tutorial.lua
@@ -79,8 +79,8 @@ end
 function TutorialStage:update(dt)
     -- update world
     self.world:update(dt)
-    print(Ball.body:getY())
-    if Ball.body:getY() > 0 then Ball:drop(nil, 30) end
+    -- print(Ball.body:getY())
+    if Ball.body:getY() > self.window.bottom then Ball:drop(nil, 30) end
 end
 
 --------------------------------------------------------------------------------
